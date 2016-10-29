f38f4c5b60aea5db965550289d2da0fbec8b110d
Zach-Attach <4589918@gmail.com>
2016-10-29 17:40:28 -0500

    .

---
+++ app/src/main/java/com/example/zyle/myapplication/Main2Activity.java
@@ -0,0 +1,16 @@
+package com.example.zyle.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
